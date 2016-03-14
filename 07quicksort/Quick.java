b9f7c99561d882fa198b8ca5112832d861ca044c
Ming You <mingyyou28@gmail.com>
2016-03-14 06:24:26 -0400

    *New QuickSort need

--- 07quicksort/Quick.java
+++ 07quicksort/Quick.java
@@ -1,11 +1,11 @@
 import java.util.*;
     public class Quick{
 	private static void swap(int[] ary, int a, int b){
-	    int temp = ary[a];
+	    int temp = ary[b];
 	    ary[b] = ary[a];
 	    ary[a] = temp;
 	}
-	private static int partition(int[]data, int left, int right){
+	private static int partitionOld(int[]data, int left, int right){
 //choose a random partition element at a location from left to right inclusive.
 	    int pivotInd = (int)(left + Math.random()*(right - left + 1));
 	    int pivotNum = data[pivotInd];
@@ -27,7 +27,7 @@ import java.util.*;
 //Quick Select will now give the kth smallest value, so the k corresponds to the index of the array! Save you one subtraction!
 
 //2.
-	public static int quickselect(int[]data, int k){
+	public static int quickselectOld(int[]data, int k){
 	    return quickselect(data,k,0,data.length -1);
 	}
 //return the kth smallest value.
@@ -36,12 +36,12 @@ import java.util.*;
 
 
 //3. If you make quickselect recursive, you need a helper method:
-    private static int quickselect(int[]data, int k, int left, int right){
+    private static int quickselectOld(int[]data, int k, int left, int right){
 	if (left == right){
 	    return data[left];
 	}
 	int pivot = partition(data,left,right);
-	if (pivot == k){
+	if (k == pivot){
 	    return data[k];}
 	else if (k < pivot){
 	    return quickselect(data,k,left, pivot - 1);
@@ -56,18 +56,87 @@ import java.util.*;
 // left <= k <= right
 //start by calling the helper as follows:
 // quickselect(data,k,0,data.length-1)
-	/*
-	private static void quickSort(int[]data){
-	    
+	
+	private static void quickSortOld(int[]data){
+	    quickSort(data, 0, data.length - 1);
 	}
-	private static void quickSort(int[]data, int left, int right){
+	private static void quickSortOld(int[]data, int left, int right){
+	    if (right > left){
+		int pivot = partition(data, left, right);
+		quickSort(data, left, pivot);
+		quickSort(data, pivot + 1, right);
+	    }
 	}
-	*/
+	
 	public static String name(){
 	    return "7,You,Ming";
 	}
+	/*
 	public static void main(String[] args){
 	    int[] ary = {4,2,6,1};
-	    System.out.println(quickselect(ary,3));
+	    System.out.println(quickselectOld(ary,0));
+	    quickSortOld(ary);
+	    System.out.println(Arrays.toString(ary));
+	}
+	*/
+	
+	private static int partition(int[]data, int left, int right){
+	    int pivotInd = (int)(left + Math.random()*(right - left + 1));
+	    int pivotNum = data[pivotInd];
+	    int leftTracker = left;
+	    swap(data,pivotInd,right);
+	    for (int i = left; i < right; i ++){
+	    	if (data[i] < pivotNum){
+		    swap(data,leftTracker, i);
+		    leftTracker+= 1;
+		}}
+	  
+	    swap(data,right,leftTracker);
+	    return leftTracker;
+	}
+
+	public static int quickselect(int[]data, int k){
+	    return quickselect(data,k,0,data.length -1);
+	}
+
+    private static int quickselect(int[]data, int k, int left, int right){
+	if (left == right){
+	    return data[left];
+	}
+	int pivot = partition(data,left,right);
+	if (k == pivot){
+	    return data[k];}
+	else if (k < pivot){
+	    return quickselect(data,k,left,pivot-1);}
+	else {
+	    return quickselect(data,k, pivot +1, right);
 	}
     }
+    
+	private static void quickSort(int[]data){
+	    quickSort(data, 0, data.length - 1);
+	}
+	private static void quickSort(int[]data, int left, int right){
+	    if (right > left){
+		int pivot = partition(data, left, right);
+		quickSort(data, left, pivot);
+		quickSort(data, pivot + 1, right);
+	    }
+	}
+//LIKE THIS:
+public static void main(String[]args){
+    int[] d = new int [4000000];
+    int[] c = new int [d.length];
+   
+    for(int i = 0; i < d.length; i++){
+	d[i]= (int)(Math.random()*Integer.MAX_VALUE);
+	c[i]= d[i];
+    }
+    quickSort(d); //or even your old quicksort!!!
+    Arrays.sort(c);
+    System.out.println("Done: Sorted="+Arrays.equals(d,c));
+}
+}
+
+
+
