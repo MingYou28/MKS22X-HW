d5e7fa64354ebafd5799f3c6dd39d0234f8a6afa
Ming You <mingyyou28@gmail.com>
2016-03-28 06:31:30 -0400

    most iterable

--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -1,49 +1,82 @@
-
-public class MyLinkedList{
+import java.util*
+public class MyLinkedList<T> implements Itereable<T>{
     Lnode head;
     int size;
-    public MyLinkedList(){
-	head = null;
-	size = 0;
-
+    
+    
     private class Lnode{
-    Lnode next;
-    int value;
-	public Lnode(int v){
-	    value = v;}
+	private T value;
+	private Lnode next;
+	public Lnode(T value){
+	    this.value = value;
+	    next = null;
+	}
+	public Lnode(T value, Lnode next){
+	    this.value = value;
+	    this.next = next;
+	}            
 	public int getValue(){
 	    return value;
 	}
 	public LNode getNext(){
 	    return next;
 	}
-	public void setValue(int v){
-	    value = v;
+	public void setValue(T value){
+	    this.value = value;
 	}
 	public void setNext(LNode n){
 	    next = n;
 	}
 	 
     }
-    
-    public int get(int index){
-	if (index > 0 && index <= size){
-	    Lnode spot = null;
-	    for (int i = 0; i < index; i ++){
-		spot = spot.getNext();}
-	    return spot.getValue();
-	}   
+    public class MyLinkedListIterator implements Iterator<T>{
+	private Lnode next;
+	public MyLinkedListIterator(){
+	next = start;
+	}
+	public boolean hasNext(){
+	    if (next != null){
+		return true;} else {
+		return false;}
+	}
+	public T next(){
+	    if (!hasNext()){
+		throw new NpSuchELementException();
+	    }
+	    T data = next.getValue();
+	    next = next.getNext();
+	    return value;
+	}
     }
+
+    private Lnode start;
+    private Lnode end;
+    private int size;
+    private T value;
+    public Iterator<T> iterator(){
+	return new MyLinkedListIterator(start);
+    }
+    public T get(int index){
+	if (index > 0 && index <= size){
+	    throw new IndextOutOfBoundsException("Pick Valid Index");
+	}
+	Lnode spot = start;
+	for (int i = 0; i < index; i ++){
+	    spot = spot.getNext();}
+	return spot.getValue();
+    }   
+}
     //- get the value of the element at the specified index (0 based)
     
-    public int set(int index,int newValue){
+    public void set(int index,int newValue){
 	if (index > 0 && index <= size){
-	    Lnode spot = null;
+	    throw new IndexOutOfBoundsException("pick valid Index");}
+	    Lnode spot = start;
 	    for (int i = 0; i < index; i ++){
 		spot = spot.getNext();}
-	    int hold = spot.getValue();
+	    T sol = spot.getValue();
 	    spot.setValue(newValue);
-	    return hold;
+	    
     }
     
     //- change the value of the element at the specified index to the newValue, return the old value
@@ -103,97 +136,3 @@ public class MyLinkedList{
     //- returns a list formatted like: [ v1, v2, v3, ... vn-1, vn ] , empty list is just []
     
 
-
-public class Driver{
-    public static void main(String[]args){
-
-        MyLinkedList<String> m = new MyLinkedList<String>();
-        ArrayList<String>    n = new ArrayList<String>();
-
-        for(int i = 0; i < 10; i++){
-            n.add(""+i);
-            m.add(""+i);
-        }
-
-        try{
-            m.add(-1,"oops");
-            System.out.println("\n\nAdd -1 #####################################");
-        }catch(IndexOutOfBoundsException e){
-
-        }
-        try{
-            m.add(12,"oops");
-            System.out.println("\n\n add 12 #####################################");
-        }catch(IndexOutOfBoundsException e){
-
-        }
-        try{
-            m.remove(12);
-            System.out.println("\n\n remove 12 #####################################");
-        }catch(IndexOutOfBoundsException e){
-
-        }
-
-        try{
-            m.set(12,"Fwazzat?!?");
-            System.out.println("\n\n set 12 #####################################");
-        }catch(IndexOutOfBoundsException e){
-
-        }
-
-//replace toString(true) with a debug to string that shows the head/tail
-        System.out.println(m.toString(true));
-        System.out.println(n);
-
-        //test removing from head/tail/middle
-        m.remove(0);
-        n.remove(0);
-        System.out.println(m.toString(true));
-
-        m.remove(2);
-        n.remove(2);
-        System.out.println(m.toString(true));
-
-        m.remove(m.size()-1);
-        n.remove(n.size()-1);
-        System.out.println(m.toString(true));
-        System.out.println(n);
-
-        //test adding to end/start
-        m.add(0,"START");
-        n.add(0,"START");
-        m.add(m.size(),"PENULTIMATE");
-        n.add(n.size(),"PENULTIMATE");
-        System.out.println(m.toString(true));
-        System.out.println(n);
-
-        //test add
-        m.add("Z-END!");
-        n.add("Z-END!");
-        System.out.println(m.toString(true));
-        System.out.println(n);
-
-        //test remove random items:
-        Random rand = new Random(0);
-        for(int i = 0; i < 6000; i++){
-            int op = rand.nextInt(4);
-
-            if(op == 0 || n.size()==0){//ensure never empty
-                n.add(""+i);
-                m.add(""+i);
-            }else if(op == 1 ){
-                int x = rand.nextInt(n.size());
-                n.add(x,""+i);
-                m.add(x,""+i);
-            }else{
-                int x = rand.nextInt(n.size());
-                if(!n.remove(x).equals(m.remove(x))){
-                    System.out.println("Non matching elements removed\n");
-                    System.exit(1);
-                }
-            }
-        }
-    }
-
-    }
-    
