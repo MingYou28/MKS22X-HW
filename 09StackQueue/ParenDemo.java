0356075c26825b7ff88fa179828b632f2025f62d
Ming You <mingyyou28@gmail.com>
2016-03-30 06:01:10 -0400

    finished
    ParenDemo

--- 09StackQueue/ParenDemo.java
+++ 09StackQueue/ParenDemo.java
@@ -1,10 +1,25 @@
 
 public class ParenDemo<T> extends MyStack<T>{
     private MyStack<T> data;
-
+    
     public static boolean isMatching(String s){
-	
-
+	MyStack<Character> opens = new MyStack<Character>();
+	for (int i=0; i<s.length(); i++) {
+	    if (Opens(s.substring(i, i + 1))){
+		opens.push(s.charAt(i));}
+	    else if (Closes(s.substring (i,i + 1))){
+		if (opens.isEmpty()){
+		    return false;
+		} else if
+			(s.charAt(i) != opens2(opens.peek())){
+			return false;
+		    }
+		    opens.pop();
+		}
+	    }    
+	return opens.isEmpty(); //ans
+    }
+	    
 	/**
 make stack
 return openings
@@ -14,16 +29,83 @@ eliminate both till left unmatched
 
 
 
-
-    public static void main(String[]args){
-    String input = "()()(([[]]))";
-    if(args.length > 0){
-	input = args[0];
-	System.out.println( isMatching(input)); 
-    }else{
-	System.out.println("Usage:"); 
-	System.out.println("java ParenDemo \"text\""); 
+    public static boolean Opens(String s){
+	if (
+	    s.equals("{")||
+	    s.equals("[")||
+	    s.equals("<")||
+	    s.equals("("))
+	    {
+		return true;		
+	    }else {
+	    return false;
+	}
     }
-    
+    public static boolean Closes(String s){
+	if (
+	    s.equals("}")||
+	    s.equals("]")||
+	    s.equals(">")||
+	    s.equals(")")){
+	    return true;		
+	    }else {
+	    return false;
+	}
+    }
+    public static char opens2(char c){
+	if (c == '<'){
+	    return '>';
+	} else if (c == '{') {
+	    return '}';
+	} else if (c == '[') {
+	    return ']';
+	} else {
+	    return ')';
+	}
+    }
+    public static void main(String[]args){
+	String input = "()()(([[]]))";
+	if(args.length > 0){
+	    input = args[0];
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
+	}    
+    }
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
