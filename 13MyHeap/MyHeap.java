86bfb4a193628e291e4d7ca647280439de4cff5c
Ming You <mingyyou28@gmail.com>
2016-05-05 13:17:54 -0400

    start heap

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+@SuppressWarnings("unchecked")
+public class MyHeap <T extends Comparable<T>>{
+    private int size;
+    private T[] data;
+    public MyHeap(){
+	data = new Comparable<T>[1];
+
+    }
+    public MyHeap(T[] array){
+	data = array;
+
+    }
+    private void pushDown(int k){
+
+    }
+    private void pushUp(int k){
+
+    }
+    private void heapify(){
+
+    }
+    public T delete(){
+
+    }
+    public void add(T x){
+
+
+    }
+    private void doubleSize(){
+
+    }
+    public String toString(){
+
+    }
+
+    //do this last
+    public MyHeap(boolean isMax){
+
+    }
+    public MyHeap(T[] array, boolean isMax){
+
+    }
+
+}
