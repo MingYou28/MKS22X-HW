c61ce0281df1d437d0877bbb5e8a5c4edffbc053
Ming You <mingyyou28@gmail.com>
2016-05-10 06:42:14 -0400

    more methods

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,17 +1,29 @@
-
-
 import java.util.*;
 @SuppressWarnings("unchecked")
 public class MyHeap <T extends Comparable<T>>{
+    
     private int size;
     private T[] data;
+    private boolean isMax;
+    
     public MyHeap(){
 	data = (T[]) new Comparable[1];
 	size = 0;
+	isMax = true;
 
     }
     public MyHeap(T[] array){
 	data = array;
+	size = array.length-1;
+	isMax = true;
+	heapify();
+
+    }
+     //do this last
+    public MyHeap(boolean isMax){
+
+    }
+    public MyHeap(T[] array, boolean isMax){
 
     }
 
@@ -38,31 +50,43 @@ public class MyHeap <T extends Comparable<T>>{
 
     }
     private void heapify(){
-
+	for (int i = size / 2; i > 0; i --){
+	    pushDown(i);
+	}
     }
     public T delete(){
 	if (size == 0){
-	} t
-	return data.remove(0);
+	    throw new NoSuchElementException();}
+	T new = data[1];
+	data[1] = data[size];
+	pushDown(1);
+	return temp;
+
     }
     public void add(T x){
-
-
+	if (size == data.length - 1){
+	    doubleSize();
+	}
+	size += 1;
+	data[size] = x;
+	pushUp(size);
     }
     private void doubleSize(){
-
+	T[] temp = (T[]) new Comparable[data.length * 2];
+	for (int i = 1; i <= size ; i ++){
+	    temp[i] = data[i];
+	}
+	data = temp;
     }
     public String toString(){
-	return "";
+	String tree = "";
+	for (int i = 1; i <= size; i ++){
+	    temp += heap[i] + " ";
+	}
+	return temp;
     }
-
-    //do this last
-    public MyHeap(boolean isMax){
-
-    }
-    public MyHeap(T[] array, boolean isMax){
-
-    }
+    
+   
 
 }
 
